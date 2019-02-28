261a9aa72067a00108722f0f5a81bbaa558357a1
alphaomega325 <alphaomega325@gmail.com>
2019-02-28 09:43:25 -0600

    Move David Wright Assignment 1 to project

---
+++ action.php
@@ -0,0 +1,27 @@
+<?php
+require_once("Template.php");
+
+$page = new Template("Congratulations");
+$page -> addHeadElement('<link rel="stylesheet" href="css/style.css">');
+$page -> addHeadElement('<meta charset="UTF-8">');
+
+$page -> finalizeTopSection();
+$page -> finalizeBottomSection();
+
+print $page -> getTopSection();
+print '<h1>Success</h1>
+
+    <nav>
+      <a href="privacy.php">Privacy Policy</a>
+      <a href="index.php">Home</a>
+    </nav>
+	
+	<article>
+	
+	<h2> Thank You </h2>
+	<p> Thank you for participating in the survey, use the nav buttons to leave away from this survey.</p>
+	
+	</article>';
+	
+print $page -> getBottomSection();
+?>
