eba51f8a885d779b062d927cb0d4b160ca20e1a6
Knippel <eknip856@uwsp.edu>
2019-03-05 09:51:10 -0600

    first change

--- albumform.php
+++ albumform.php
@@ -18,5 +18,24 @@ print "
 
 ";
 
+$db = new DB();
+
+//var_dump($db);
+
+$search = searchInput;
+
+if (!$db->getConnStatus()) {
+  print "An error has occurred with connection\n";
+  exit;
+}
+
+$query = "SELECT Album, Artist
+        FROM albuminfo
+        WHERE Album = $search OR ARTIST = $search";
+
+$result = $db->dbCall($query);
+
+var_dump($result);
+
 print $page->getBottomSection();
 ?>
