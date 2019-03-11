55f0020543703195f6d35d2128b196bb1b3cd108
alphaomega325 <alphaomega325@gmail.com>
2019-03-11 16:13:09 -0500

    Did the Data Sanitization

--- album_result.php
+++ album_result.php
@@ -1,4 +1,17 @@
 <?php
 
+require_once("DB.class.php");
+
+$db = new DB();
+
+$album_or_artist = $_POST;
+
+//Validation goes here
+
+
+//Data Sanitization goes here
+$album_or_artist = $db->dbEsc($album_or_artist);
+
+//Resulting sheet goes here
 
 ?>
