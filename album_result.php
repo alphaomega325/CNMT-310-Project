e340f392a904d22e6544037ea33dd17d765ab254
alphaomega325 <alphaomega325@gmail.com>
2019-03-12 09:50:37 -0500

    Changed and added the functions for search pages

--- album_result.php
+++ album_result.php
@@ -1,19 +1,36 @@
 <?php
+//Setup for Webpage
+require_once("Template.php");
+$page = new Template("Album Result");
+$page -> addHeadElement('<link rel="stylesheet" href="css/style.css">');
+$page -> addHeadElement('<meta charset="UTF-8">');
+$page -> finalizeTopSection();
+$page -> finalizeBottomSection();
+
+//Database Class setup
+
 require_once("DB.class.php");
 
 $db = new DB();
 
+if (!$db->getConnStatus()) {
+  print "An error has occurred with connection\n";
+  exit;
+}
+
 $search = $_POST["searchInput"];
 
-//Validation goes here
 
 
 //Data Sanitization goes here
 $search = $db->dbEsc($search);
 
-if (!$db->getConnStatus()) {
-  print "An error has occurred with connection\n";
-  exit;
+
+//Validation goes here
+
+if(!isset($search))
+{
+    searchFailurePage($page);
 }
 
 $query = "SELECT Album, Artist
@@ -22,41 +39,53 @@ $query = "SELECT Album, Artist
 
 $result = $db->dbCall($query);
 
-//Resulting sheet goes here
+
+successPage($page);
+
+//Resulting WebPages goes here
+
+function searchFailurePage(Template $page){
+    print $page -> getTopSection();
+
+    print"<h1>Failure<h1>
+
+    <p>We have failed to detect any items in the search bar.  Please try again later.</p>
 
 
+";
 
-require_once("Template.php");
-$page = new Template("Album Result");
-$page -> addHeadElement('<link rel="stylesheet" href="css/style.css">');
-$page -> addHeadElement('<meta charset="UTF-8">');
-$page -> finalizeTopSection();
-$page -> finalizeBottomSection();
-	
-print $page -> getTopSection();
+    print $page->getBottomSection();
 
-print "<h1>Success</h1>
+
+}
+
+
+function successPage(Template $page){	
+    print $page -> getTopSection();
+    
+    print "<h1>Success</h1>
 
     <nav>
       <a href="privacy.php">Privacy Policy</a>
       <a href="index.php">Home</a>
       <a href="albumform.php">Album Form</a>
     </nav>"
-	
-print $result;
+                        
+    print $result;
+                        
+    print '<table>';
+   
+    foreach($result as $album){
+        print "<tr>";
+        print "<td>".$album['id']."</td>";
+        print "<td>".$album['albumtitle']."</dt>";
+        print "<td>".$album['albumartist']."</dt>";	
+        print "<td>".$album['albumlength']."</dt>";	
+        print "</tr>";
+    }
 
-print '<table>';
+    print '</table>';
 
-foreach($result as $album){
-	print "<tr>";
-	print "<td>".$album['id']."</td>";
-	print "<td>".$album['albumtitle']."</dt>";
-	print "<td>".$album['albumartist']."</dt>";	
-	print "<td>".$album['albumlength']."</dt>";	
-	print "</tr>";
+    print $page ->getBottomSection();
 }
-
-print '</table>';
-
-print $page ->getBottomSection();
 ?>
